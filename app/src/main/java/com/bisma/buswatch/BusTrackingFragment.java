101ff2dd45e25f2bc380d4361a230d38cb11e713
BisMac <Bisma@69-98.priv23.nus.edu.sg>
2016-10-27 14:25:06 +0800

    Adding map function. Unsure of capability. Under RealTimeETAFragment the column to choose bus type is added

--- app/src/main/java/com/bisma/buswatch/BusTrackingFragment.java
+++ app/src/main/java/com/bisma/buswatch/BusTrackingFragment.java
@@ -6,8 +6,27 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+
 
 public class BusTrackingFragment extends Fragment{
+
+    private MapView mapView;
+    private GoogleMap map;
+
 
     public BusTrackingFragment() {
         // Required empty public constructor
@@ -23,7 +42,14 @@ public class BusTrackingFragment extends Fragment{
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_bus_tracking, container, false);
+
+        // Gets the MapView from the XML layout and creates it
+        mapView = (MapView) v.findViewById(R.id.mapview);
+        mapView.onCreate(savedInstanceState);
+
+        // Gets to GoogleMap from the MapView and does initialization stuff
+        map = mapView.getMap();
+
         return v;
     }
-
 }
